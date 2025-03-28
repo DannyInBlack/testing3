41ff71cc1f174f4c8138b41e7fe616e856677b87
Danial Ghobrial <danielghobrial11@gmail.com>
2025-03-28 04:37:30 +0200

    wow

--- cypress/e2e/7th.cy.js
+++ cypress/e2e/7th.cy.js
@@ -1,26 +1,21 @@
-describe.only('Invalidated Access', () => {
+describe('Invalidated Access', () => {
   before(() => {
-    cy.fixture("example")
-      .then((data) => {
-        globalThis.users = data.users;
-        globalThis.passwords = data.passwords;
-        globalThis.site = data.site;
-      })
+    cy.exampleFixture();
   })
 
-  beforeEach('Must open login page first to get the error', () => {
+  beforeEach('[0] Must open login page first to get the error', () => {
     cy.matchUT(site.url, site.title);
   })
 
   // Each of these urls should return to the login page with an error.
-  afterEach('Check current page is the login page', () => {
+  afterEach('[*] Check current page is the login page', () => {
     cy.url().should('eq', site.url);
     cy.title().should('include', site.title);
   })
 
   it("[1] Trying to access inventory page", () => {
-    cy.visit(site.url, site.pages.inventory, { failOnStatusCode: false });
-    cy.errorText(`Epic sadface: You can only access '/${site.pages.inventory}' when you are logged in`);
+    cy.visit(site.url + site.pages.inventory, { failOnStatusCode: false });
+    // cy.errorText(`Epic sadface: You can only access '/${site.pages.inventory}' when you are logged in`);
   })
 
   it("[2] Trying to access cart page", () => {
@@ -38,116 +33,188 @@ describe.only('Invalidated Access', () => {
     cy.errorText(`Epic sadface: You can only access '/${site.pages.checkout2}' when you are logged in`);
   })
 
-  it("[6] Trying to access checkout complete page", () => { 
+  it("[5] Trying to access checkout complete page", () => { 
     cy.visit(site.url + site.pages.checkout3, { failOnStatusCode: false });
     cy.errorText(`Epic sadface: You can only access '/${site.pages.checkout3}' when you are logged in`);
   })
 });
 
-// describe.skip('Login Page', () => {
-//   beforeEach('Visit the page url', () => {
-//     cy.matchUT(url, title);
-//   })
+describe('Login Page', () => {
+  before(() => {
+    cy.exampleFixture();
+  })
   
-//   // missing username and password
-//   it("[1] Missing username and password", () => {
-//     cy.login();
-//     cy.url().should('eq', url);
-//     cy.loginError()
-//     cy.errorText("Epic sadface: Username is required");
-//   })
+  beforeEach('[0] Visit the page url', () => {
+    // cy.clearCookies();
+    cy.matchUT(site.url, site.title);
+  })
   
-//   // missing password
-//   it("[2] Missing password", () => {
-//     cy.user(validUser);
-//     cy.login();
-//     cy.url().should('eq', url);
-//     cy.loginError();
-//     cy.errorText("Epic sadface: Password is required");
-//   })
+  // missing name and password
+  it("[1] Missing username and password", () => {
+    cy.login();
+    cy.url().should('eq', site.url);
+    cy.loginError()
+    cy.errorText("Epic sadface: Username is required");
+  })
   
-//   // username and password do not match any user in this service
-//   it("[3] Incorrect username and password", () => {
-//     cy.user(wrongUser);
-//     cy.pass(wrongPass);
-//     cy.login();
-//     cy.url().should('eq', url);
-//     cy.loginError();
-//     cy.errorText("Epic sadface: Username and password do not match any user in this service");
-//   })
+  // missing password
+  it("[2] Missing password", () => {
+    cy.user(users.valid);
+    cy.login();
+    cy.url().should('eq', site.url);
+    cy.loginError();
+    cy.errorText("Epic sadface: Password is required");
+  })
   
-//   // password doesn't match
-//   it("[4] Incorrect password", () => {
-//     cy.user(validUser);
-//     cy.pass(wrongPass);
-//     cy.login();
-//     cy.url().should('eq', url);
-//     cy.loginError();
-//     cy.errorText("Epic sadface: Username and password do not match any user in this service");
-//   })
+  // username and password do not match any user in this service
+  it("[3] Incorrect username and password", () => {
+    cy.user(users.wrong);
+    cy.pass(passwords.wrong);
+    cy.login();
+    cy.url().should('eq', site.url);
+    cy.loginError();
+    cy.errorText("Epic sadface: Username and password do not match any user in this service");
+  })
   
-//   // valid user and password
-//   it("[5] Valid user", () => {
-//     cy.user(validUser);
-//     cy.pass(pass);
-//     cy.login();
-//     cy.wait(1000);
-//     cy.url().should('eq', url + pages[0]);
-//   })
+  // password doesn't match
+  it("[4] Incorrect password", () => {
+    cy.user(users.valid);
+    cy.pass(passwords.wrong);
+    cy.login();
+    cy.url().should('eq', site.url);
+    cy.loginError();
+    cy.errorText("Epic sadface: Username and password do not match any user in this service");
+  })
   
-//   // blocked user
-//   it("[6] Blocked user", () => {
-//     cy.user(blockedUser);
-//     cy.pass(pass);
-//     cy.login();
-//     cy.url().should('eq', url);
-//     cy.loginError();
-//     cy.errorText("Epic sadface: Sorry, this user has been locked out.");
-//   })
+  // valid user and password
+  it("[5] Valid user", () => {
+    cy.user(users.valid);
+    cy.pass(passwords.correct);
+    cy.login();
+    cy.wait(1000);
+    cy.url().should('eq', site.url + site.pages.inventory);
+  })
   
-// });
+  // blocked user
+  it("[6] Blocked user", () => {
+    cy.user(users.blocked);
+    cy.pass(passwords.correct);
+    cy.login();
+    cy.url().should('eq', site.url);
+    cy.loginError();
+    cy.errorText("Epic sadface: Sorry, this user has been locked out.");
+  })
+  
+});
 
-// describe.skip('Products Page', () => {
-//   beforeEach('Login with the provided user', () => {
-//     cy.matchUT(url, title);
-//     cy.user(validUser);
-//     cy.pass(pass);
-//     cy.login();
-//     cy.wait(1000);
-//     cy.url().should('eq', url + pages[0]);
-//   });
+describe.only('Products Page', () => {
 
-//   it('[1] Product has correct image', () => {
-//     cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
-//       .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
-//   });
+  before(() => {
+    cy.exampleFixture();
+  })
 
-// });
+  beforeEach('Login with the provided user', () => {
+    cy.matchUT(site.url, site.title);
+    cy.user(users.performance);
+    cy.pass(passwords.correct);
+    cy.login();
+    cy.url().should('eq', site.url + site.pages.inventory);
+  });
 
-// describe('Problem User', () => {
-//   // All product images are replaced with a dog image
-//   // Cannot add any products 
-//   // Cannot edit last name in
-//   // cannot change sorting method 
-// });
+  // error that can occur with visual error user
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+
+  // Menu, close menu icon is slightly tilted
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+
+  // Cart icon appeasr on top of the filter icon
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Bottom right add to cart button is shifted to the right
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Middle row product names are shifted to the right
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Prices are randomly generated on each reload/navigation
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Cannot add any products
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Cannot click twice to add and remove items from cart
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Cannot change sorting method 
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Trying to sort will not work, causing an alert to pop up
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Cannot go to cart page
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+});
+
+describe('Checkout Page 1', () => {
+
+  // Cannot edit last name in 
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  // Last name isn't a required field in checkout form
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+  
+  
+})
+
+describe('Checkout Page 2', () => {
+  //   // Cannot finish checkout to go to order details page
+  it('[1] Product has correct image', () => {
+    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
+      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  });
+
+})
 
 // describe('Performance Glitch User', () => {
 //   // Takes more than 3 seconds to redirect to the inventory page
 // });
-
-// describe('Visual Error User', () => {
-//   // Cart icon appeasr on top of the filter icon
-//   // Menu, close menu icon is slightly tilted
-//   // Bottom right add to cart button is shifted to the right
-//   // Prices are randomly generated on each reload/navigation
-//   // Backpack image is incorrect
-//   // Middle row product names are shifted to the right
-// });
-
-// describe('Error User', () => {
-//   // Cannot click twice to add and remove items from cart
-//   // Trying to sort will not work, causing an alert to pop up
-//   // Last name isn't a required field in checkout form
-//   // Cannot finish checkout to go to order details page
-// });
-
--- cypress/support/urls.js
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add('matchUT', (url, title) => {
-  cy.visit(url);
-  cy.url().should('eq', url);
-  cy.title().should('include', title);
-})
