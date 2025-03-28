37bc86bf6b5ea8242b01feff7c769dc03166b848
Danial Ghobrial <danielghobrial11@gmail.com>
2025-03-28 22:28:16 +0200

    Finished 7th project

--- cypress/e2e/7th.cy.js
+++ cypress/e2e/7th.cy.js
@@ -36,7 +36,7 @@ describe('Invalidated Access', () => {
     cy.errorText(`Epic sadface: You can only access '/${site.pages.checkout2}' when you are logged in`);
   })
 
-  it("[5] Trying to access checkout complete page", () => { 
+  it("[5] Trying to access checkout complete page", () => {
     cy.visit(site.url + site.pages.checkout3, { failOnStatusCode: false });
     cy.errorText(`Epic sadface: You can only access '/${site.pages.checkout3}' when you are logged in`);
   })
@@ -46,12 +46,12 @@ describe('Login Page', () => {
   before(() => {
     cy.exampleFixture();
   })
-  
+
   beforeEach('[0] Visit the page url', () => {
     // cy.clearCookies();
     cy.matchUT(site.url, site.title);
   })
-  
+
   // missing name and password
   it("[1] Missing username and password", () => {
     cy.login();
@@ -59,7 +59,7 @@ describe('Login Page', () => {
     cy.loginError()
     cy.errorText("Epic sadface: Username is required");
   })
-  
+
   // missing password
   it("[2] Missing password", () => {
     cy.user(users.valid);
@@ -68,7 +68,7 @@ describe('Login Page', () => {
     cy.loginError();
     cy.errorText("Epic sadface: Password is required");
   })
-  
+
   // username and password do not match any user in this service
   it("[3] Incorrect username and password", () => {
     cy.user(users.wrong);
@@ -78,7 +78,7 @@ describe('Login Page', () => {
     cy.loginError();
     cy.errorText("Epic sadface: Username and password do not match any user in this service");
   })
-  
+
   // password doesn't match
   it("[4] Incorrect password", () => {
     cy.user(users.valid);
@@ -88,96 +88,235 @@ describe('Login Page', () => {
     cy.loginError();
     cy.errorText("Epic sadface: Username and password do not match any user in this service");
   })
-  
+
   // valid user and password
   it("[5] Valid user", () => {
     cy.user(users.valid);
-    cy.pass(passwords.correct);
+    cy.pass(passwords.valid);
     cy.login();
     cy.wait(1000);
     cy.url().should('eq', site.url + site.pages.inventory);
   })
-  
+
   // blocked user
   it("[6] Blocked user", () => {
     cy.user(users.blocked);
-    cy.pass(passwords.correct);
+    cy.pass(passwords.valid);
     cy.login();
     cy.url().should('eq', site.url);
     cy.loginError();
     cy.errorText("Epic sadface: Sorry, this user has been locked out.");
   })
-  
+
 });
 
-describe.only('Products Page', () => {
+describe('Products Page', () => {
 
   before(() => {
     cy.exampleFixture();
   })
 
-  beforeEach('Login with the provided user', () => {
+  beforeEach('[0] Login with the provided user', () => {
     cy.matchUT(site.url, site.title);
-    cy.user(users.performance);
-    cy.pass(passwords.correct);
+    cy.user(users.valid);
+    cy.pass(passwords.valid);
     cy.login();
     cy.url().should('eq', site.url + site.pages.inventory);
   });
 
-  // error that can occur with visual error user
+  // Product images are correct
   it('[1] Product has correct image', () => {
     cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
       .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
   });
 
-  // Menu, close menu icon is slightly tilted
+  // Menu, close menu icon have correct orientation
   it('[2] Menu icons have correct rotation', () => {
     cy.get('[data-test=open-menu]').should('not.have.class', 'visual_failure');
-    cy.get('#react-burger-menu-icon').click();
+    cy.get('#react-burger-menu-btn').click();
     cy.get('[data-test=close-menu]').should('not.have.class', 'visual_failure');
   });
 
-  // Cart icon appeasr on top of the filter icon
+  // Cart icon is aligned correctly
   it('[3] Cart icon in correct position', () => {
     cy.get('#shopping_cart_container').should('not.have.class', 'visual_failure');
   });
-  
-  // Bottom right add to cart button is shifted to the right
+
+  // Cart buttons are aligned correctly
   it('[4] Product has correct image', () => {
     cy.get('.btn_inventory')
       .each(button => {
         expect(button).to.not.have.class('btn_inventory_misaligned')
-      })
+      });
   });
-  
-  // // Middle row prodj
-  
+
+  // Product labels are aligned correctly
+  it('[5] Product names are aligned correctly', () => {
+    cy.get('[data-test=inventory-item-name]')
+      .each(label => {
+        expect(label).to.not.have.class('align_right')
+      });
+  });
+
+  // Can change sorting method
+  it('[6] Change sorting method', () => {
+    cy.get('[data-test=product-sort-container').select('lohi');
+    cy.get('[data-test=inventory-item-name]').eq(0).should('have.text', 'Sauce Labs Onesie');
+
+    cy.get('[data-test=product-sort-container').select('hilo');
+    cy.get('[data-test=inventory-item-name]').eq(0).should('have.text', 'Sauce Labs Fleece Jacket');
+
+    cy.get('[data-test=product-sort-container').select('za');
+    cy.get('[data-test=inventory-item-name]').eq(0).should('have.text', 'Test.allTheThings() T-Shirt (Red)');
+  });
+
+  // Can add products to the cart
+  it('[7] Add product to cart', () => {
+    cy.get('#remove-sauce-labs-backpack').should('not.exist');
+    cy.get('#add-to-cart-sauce-labs-backpack').click();
+    cy.get('#add-to-cart-sauce-labs-backpack').should('not.exist');
+    cy.get('#remove-sauce-labs-backpack').should('exist');
+    cy.get('[data-test=shopping-cart-badge]').should('include.text', '1');
+    cy.get('[data-test=shopping-cart-link]').click();
+    cy.url().should('include', site.pages.cart);
+    cy.contains('Sauce Labs Backpack').should('exist');
+  });
+
+  // Can remove products from the cart
+  it('[8] Add and remove an item from cart', () => {
+    cy.get('#remove-sauce-labs-backpack').should('not.exist');
+    cy.get('#add-to-cart-sauce-labs-backpack').click();
+    cy.get('#add-to-cart-sauce-labs-backpack').should('not.exist');
+    cy.get('#remove-sauce-labs-backpack').should('exist');
+    cy.get('[data-test=shopping-cart-badge]').should('include.text', '1');
+
+    cy.get('[data-test=shopping-cart-link]').click();
+    cy.url().should('include', site.pages.cart);
+    cy.contains('Sauce Labs Backpack').should('exist');
+
+    cy.get('#continue-shopping').click();
+    cy.url().should('include', site.pages.inventory);
+
+    cy.get('#remove-sauce-labs-backpack').click();
+    cy.get('#remove-sauce-labs-backpack').should('not.exist');
+    cy.get('#add-to-cart-sauce-labs-backpack').should('exist');
+    cy.get('[data-test=shopping-cart-badge]').should('not.exist');
+
+    cy.get('[data-test=shopping-cart-link]').click();
+    cy.url().should('include', site.pages.cart);
+    cy.contains('Sauce Labs Backpack').should('not.exist');
+  });
+
+  // Can open product page
+  it('[9] Open product page', () => {
+    cy.get('#item_4_title_link').click();
+
+    cy.url().should('include', site.pages.product);
+
+    cy.get('[data-test=inventory-item-name]')
+      .should('include.text', 'Sauce Labs Backpack');
+  });
 });
 
 describe('Checkout Page 1', () => {
 
-  // Cannot edit last name in 
-  it('[1] Product has correct image', () => {
-    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
-      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  before(() => {
+    cy.exampleFixture();
+  });
+
+  beforeEach('[0] Login then add product to cart', () => {
+    cy.matchUT(site.url, site.title);
+    cy.user(users.valid);
+    cy.pass(passwords.valid);
+    cy.login();
+    cy.url().should('eq', site.url + site.pages.inventory);
+
+    cy.get('#add-to-cart-sauce-labs-backpack').click();
+    cy.get('[data-test=shopping-cart-link]').click();
+    cy.url().should('include', site.pages.cart);
+    cy.contains('Sauce Labs Backpack').should('exist');
+    
+    cy.get('#checkout').click();
+    cy.url().should('include', site.pages.checkout1);
+  });
+
+  // Cannot submit with filling in all form fields. 
+  it('[1] All fields missing', () => {
+    cy.get('#continue').click();
+    cy.url().should('include', site.pages.checkout1);
+    cy.get('.error-message-container').should('include.text', 'Error: First Name is required');
   });
   
-  // Last name isn't a required field in checkout form
-  it('[1] Product has correct image', () => {
-    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
-      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  it('[2] Firstname required', () => {
+    // cy.get('#first-name').type('danny');
+    cy.get('#last-name').type('is the man');
+    cy.get('#postal-code').type('you wish');
+    cy.get('#continue').click();
+    cy.url().should('include', site.pages.checkout1);
+    cy.get('.error-message-container').should('include.text', 'Error: First Name is required');
   });
   
+  it('[3] Lastname required', () => {
+    cy.get('#first-name').type('danny');
+    // cy.get('#last-name').type('is the man');
+    cy.get('#postal-code').type('you wish');
+    cy.get('#continue').click();
+    cy.url().should('include', site.pages.checkout1);
+    cy.get('.error-message-container').should('include.text', 'Error: Last Name is required');
+  });
   
+  it('[4] Postal code required', () => {
+    cy.get('#first-name').type('danny');
+    cy.get('#last-name').type('is the man');
+    // cy.get('#postal-code').type('you wish');
+    cy.get('#continue').click();
+    cy.url().should('include', site.pages.checkout1);
+    cy.get('.error-message-container').should('include.text', 'Error: Postal Code is required');
+  });
+
 })
 
 describe('Checkout Page 2', () => {
   //   // Cannot finish checkout to go to order details page
-  it('[1] Product has correct image', () => {
-    cy.get('[data-test=inventory-item-sauce-labs-backpack-img]')
-      .should('have.attr', 'src', '/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg');
+  before(() => {
+    cy.exampleFixture();
   });
 
+  beforeEach('[0] Login, add product to cart, checkout', () => {
+    cy.matchUT(site.url, site.title);
+    cy.user(users.valid);
+    cy.pass(passwords.valid);
+    cy.login();
+    cy.url().should('eq', site.url + site.pages.inventory);
+
+    cy.get('#add-to-cart-sauce-labs-backpack').click();
+    cy.get('[data-test=shopping-cart-link]').click();
+    cy.url().should('include', site.pages.cart);
+    cy.contains('Sauce Labs Backpack').should('exist');
+    
+    cy.get('#checkout').click();
+    cy.url().should('include', site.pages.checkout1);
+
+    cy.get('#first-name').type('danny');
+    cy.get('#last-name').type('is the man');
+    cy.get('#postal-code').type('you wish');
+
+    cy.get('#continue').click();
+    cy.url().should('include', site.pages.checkout2);
+
+  });
+
+  it('[1] Checkout includes cart item', () => {
+    cy.contains('Sauce Labs Backpack').should('exist');
+  })
+
+  it('[2] Finalize checkout', () => {
+    cy.get('#finish').click();
+    cy.url().should('include', site.pages.checkout3);
+
+    cy.get('#back-to-products').click();
+    cy.url().should('include', site.pages.inventory);
+  })
 })
 
 // describe('Performance Glitch User', () => {
--- cypress/e2e/intercept.cy.js
+++
@@ -1,6 +0,0 @@
-describe("Handle network intercept", () => {
-  it('[1] intercept network between FE request and BE', () => {
-    cy.visit('https://thinking-tester-contact-list.herokuapp.com/adduser')
-
-  })
-})
